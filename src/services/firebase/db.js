56c646e15a532de875ea9937129b99e758d3e39c
dante perrone <dantep2015@gmail.com>
2024-11-17 04:20:17 -0300

    trabajo finalizado creeria

--- src/services/firebase/db.js
+++ src/services/firebase/db.js
@@ -12,9 +12,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-
-export const db = getFirestore(app)
-export const itemskey = 'items'
-
-
-
+export const db = getFirestore(app);
+export const itemskey = 'items';
